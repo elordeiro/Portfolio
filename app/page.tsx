106f03ba0d58ca7e9054d1fd630240b6a361d766
elordeiro <estevao.lordeiro@hotmail.com>
2024-01-27 01:33:50 -0500

    Fix window undefined error

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,26 @@ import { ProjectProps, Projects } from "./Projects";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-    const [width, setWidth] = useState(window.innerWidth);
+    const [width, setWidth] = useState<number | undefined>(undefined);
 
     const handleWindowResize = () => {
         setWidth(window.innerWidth);
     };
 
     useEffect(() => {
-        window.addEventListener("resize", handleWindowResize);
+        // Check if window is defined (it won't be during SSR)
+        if (typeof window !== "undefined") {
+            // Set width to initial client-side width
+            setWidth(window.innerWidth);
 
-        return () => {
-            window.removeEventListener("resize", handleWindowResize);
-        };
+            // Add event listener for window resize
+            window.addEventListener("resize", handleWindowResize);
+
+            // Cleanup on unmount
+            return () => {
+                window.removeEventListener("resize", handleWindowResize);
+            };
+        }
     }, []);
 
     return (
